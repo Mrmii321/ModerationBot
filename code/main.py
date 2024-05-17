5bc688d71f4ee21dd375cad0bbfffc2a8df07f96
soapchan <noahfoxgaming@gmail.com>
2024-05-17 13:46:01 +1000

    done a fuck ton of stuff, just doing this to keep it safe.

--- code/main.py
+++ code/main.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import time
 
 import discord
 from discord.ext import commands, tasks
@@ -12,7 +13,8 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 class Main:
     def __init__(self, ai_key, bot_class_var):
-        self.status = discord.Game(name="Type =help")
+        self.status = discord.Activity(type=discord.ActivityType.watching, name="for =help", start=0)
+        # start=0 is the timestamp for when activity starts
         self.client = OpenAI(api_key=ai_key)
         self.bot = bot_class_var
 
@@ -44,7 +46,7 @@ class MariaDB:
         self.database = "tybalt-logs"
 
 
-    def log_filter(self, message, author, channel, time_sent):
+    def log_filter(self, message, author, channel, time_sent, harmful_word):
         try:
             db = mariadb.connect(
                 host=self.host,
@@ -55,14 +57,15 @@ class MariaDB:
 
             cursor = db.cursor()
 
-            insert_into_table = (f"""INSERT INTO messages (message, author, channel, time_sent)
-                                VALUES ('{message}', '{author}', '{channel}', '{time_sent}');
+            insert_into_table = (f"""INSERT INTO messages (message, author, channel, time_sent, word)
+                                VALUES ('{message}', '{author}', '{channel}', '{time_sent}', '{harmful_word}');
                                 """)
             logging.info(f"Run database command {insert_into_table}")
 
             cursor.execute(insert_into_table)
             db.commit()
             db.close()
+            logging.info(f"Ran bad word database command {insert_into_table}")
         except mariadb.Error as e:
             logging.error(e)
 
@@ -81,7 +84,7 @@ class MariaDB:
             insert_into_table = (f"""INSERT INTO ai_messages (message, author, channel, time_sent, flags)
                                 VALUES ('{message}', '{author}', '{channel}', '{time_sent}', '{flags}');
                                 """)
-            logging.info(f"Run database command {insert_into_table}")
+            logging.info(f"Ran AI database command {insert_into_table}")
 
             cursor.execute(insert_into_table)
             db.commit()
@@ -101,6 +104,7 @@ def setup_bot():
     bypass_roles = ["Owner", "Admin", "General Manager", "Community manager", "Staff manager",
                     "Events Manager", "Consultant", "Senior Moderator"]
     debug_role = ["bot debug perms"]
+    start_time = int(time.time())
 
     main = Main(ai_key, bot)
 
@@ -113,8 +117,14 @@ def setup_bot():
     @bot.event
     async def on_ready():
         """Logs in the bot."""
+        tybalt_logs = bot.get_channel(982548416376750100)
         logging.info(f"Logged in as {bot.user.name}")
         await bot.change_presence(activity=main.status)
+        message = (
+            f"AutoMod started up at <t:{start_time}>"
+        )
+        embed = discord.Embed(description=message, color=discord.Color.green(), title="**AutoMod Online**")
+        await tybalt_logs.send(embed=embed)
 
 
     @bot.event
@@ -123,7 +133,7 @@ def setup_bot():
         user_roles = [role.name for role in message.author.roles]
 
         if not any(role in user_roles for role in bypass_roles) or any(role in user_roles for role in debug_role):
-            if message.author == bot.user or message.author == bot.user:
+            if message.author == bot.user or message.author == discord.Message.bot:
                 return
 
             flagged_categories = await main.get_flagged_categories(text=message.content)
@@ -163,7 +173,8 @@ def setup_bot():
                     maria_db.log_filter(message=message.content,
                                         author=message.author,
                                         channel=message.channel,
-                                        time_sent=message.created_at)
+                                        time_sent=message.created_at,
+                                        harmful_word=word)
                     await message.delete()
 
             else:
@@ -193,13 +204,13 @@ def setup_bot():
 
 
 
-    @bot.command()
-    async def spamcheck(ctx):
+    @bot.command(name="spamcheck")
+    async def check_for_spam_warnings(ctx):
         """Check the server for any potential spammers"""
         await check_for_spammers()
 
 
-    @bot.command()
+    @bot.command(name="myflags")
     async def check_self_flags(ctx):
         """Checks your flags"""
         flag_list = []
@@ -209,6 +220,17 @@ def setup_bot():
         for flag in flags:
             flag_list.append(flag)
         await ctx.send(flag_list)
+
+
+    @bot.command(name="uptime")
+    async def uptime(ctx):
+
+        message = (
+            f"AutoMod has been online for <t:{start_time}:R>"
+        )
+
+        embed = discord.Embed(description=message, color=discord.Color.green(), title="Uptime")
+        await ctx.send(embed=embed)
 
 
     async def send_message(channel_id, message):
