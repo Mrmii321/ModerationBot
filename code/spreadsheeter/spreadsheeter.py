c83a7be1117cdd52288ac47deddee832c26bf516
soapchan <noahfoxgaming@gmail.com>
2024-05-20 16:22:08 +1000

    Fixed tpos and added nonowords.json to gitignore

--- code/spreadsheeter/spreadsheeter.py
+++ code/spreadsheeter/spreadsheeter.py
@@ -11,6 +11,5 @@ class Spreadsheeter:
     def add_role(self, role, user):
         logging.info(f"spreadsheeter added {role} to {user}")
 
-
     def remove_role(self, role, user):
         logging.info(f"spreadsheeter removed {role} from {user}")
